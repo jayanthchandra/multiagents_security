bf03bf205bf8d2267db38479c9398df7c9c4210b
Trisha Venkatesh <trishav96@gmail.com>
2017-05-31 18:37:05 +0530

    First version of code for generating key pair

---
+++ MakeKeys.java
@@ -0,0 +1,203 @@
+import java.io.*;
+
+import java.security.*;
+
+import java.security.spec.*;
+
+
+
+public class MakeKeys {
+
+
+
+
+ public static void main(String args[]) {
+
+  MakeKeys MakeKeys = new MakeKeys();
+
+  try {
+
+   String path = "C://Users/trish/Desktop/run";
+
+
+
+
+   KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA");
+
+
+
+
+   keyGen.initialize(1024); 
+
+   KeyPair g_keypair = keyGen.genKeyPair();
+
+
+
+
+   System.out.println("The generated key pair:");
+
+   MakeKeys.dump_kp(g_keypair);
+
+   MakeKeys.save_kp(path, g_keypair);
+
+
+
+
+   KeyPair l_keypair = MakeKeys.LoadKeyPair(path, "DSA");
+
+   System.out.println("Key Pair Loaded into file");
+
+   MakeKeys.dump_kp(l_keypair);
+
+  } catch (Exception e) {
+
+   e.printStackTrace();
+
+   return;
+
+  }
+
+ }
+
+
+
+
+ private void dump_kp(KeyPair keyPair) {
+
+  PublicKey pub = keyPair.getPublic();
+
+  System.out.println("Public Key: " + get_hex(pub.getEncoded()));
+
+
+
+
+  PrivateKey priv = keyPair.getPrivate();
+
+  System.out.println("Private Key: " + get_hex(priv.getEncoded()));
+
+ }
+
+
+
+
+ private String get_hex(byte[] b) {
+
+  String result = "";
+
+  for (int i = 0; i < b.length; i++) {
+
+   result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+
+  }
+
+  return result;
+
+ }
+
+
+
+
+ public void save_kp(String path, KeyPair keyPair) throws IOException {
+
+  PrivateKey privateKey = keyPair.getPrivate();
+
+  PublicKey publicKey = keyPair.getPublic();
+
+
+
+
+  // Store Public Key.
+
+  X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+
+    publicKey.getEncoded());
+
+  FileOutputStream fos = new FileOutputStream(path + "/public_key");
+
+  fos.write(x509EncodedKeySpec.getEncoded());
+
+  fos.close();
+
+
+
+
+  // Store Private Key.
+
+  PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+
+    privateKey.getEncoded());
+
+  fos = new FileOutputStream(path + "/private_key");
+
+  fos.write(pkcs8EncodedKeySpec.getEncoded());
+
+  fos.close();
+
+ }
+
+
+
+
+ public KeyPair LoadKeyPair(String path, String algorithm)
+
+   throws IOException, NoSuchAlgorithmException,
+
+   InvalidKeySpecException {
+
+  // Read Public Key.
+
+  File filePublicKey = new File(path + "/public_key");
+
+  FileInputStream fis = new FileInputStream(path + "/public_key");
+
+  byte[] encodedPublicKey = new byte[(int) filePublicKey.length()];
+
+  fis.read(encodedPublicKey);
+
+  fis.close();
+
+
+
+
+  // Read Private Key.
+
+  File filePrivateKey = new File(path + "/private_key");
+
+  fis = new FileInputStream(path + "/private_key");
+
+  byte[] encodedPrivateKey = new byte[(int) filePrivateKey.length()];
+
+  fis.read(encodedPrivateKey);
+
+  fis.close();
+
+
+
+
+  // Generate KeyPair.
+
+  KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+
+  X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(
+
+    encodedPublicKey);
+
+  PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
+
+
+
+
+  PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(
+
+    encodedPrivateKey);
+
+  PrivateKey privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+
+
+
+  return new KeyPair(publicKey, privateKey);
+
+ }
+
+}
